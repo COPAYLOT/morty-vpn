2502fc667e031ca591d65eefbcc983954fb63c82
sergeyhatunzev <sergeyhatunzev@gmail.com>
2026-07-09 17:53:30 +0500

    fix(proton): import io.ktor.client.request.header

--- app/src/main/java/com/zaneschepke/wireguardautotunnel/domain/service/ProtonConfigService.kt
+++ app/src/main/java/com/zaneschepke/wireguardautotunnel/domain/service/ProtonConfigService.kt
@@ -15,6 +15,7 @@ import com.zaneschepke.wireguardautotunnel.util.extensions.saveTunnelsUniquely
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.client.request.headers
 import io.ktor.client.request.parameter
 import io.ktor.client.request.post
